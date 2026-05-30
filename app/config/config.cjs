b4ebf3d52beac1d5e1c5cabf305eab9fcf886313
Danis Arslanov <arslanovdk@gmail.com>
2026-05-30 13:32:02 +0000

    add PostgreSQL service and update configuration

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -14,15 +13,11 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
